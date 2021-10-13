af51bd5c9988a650ec5a821f7610b5395d04eba6
Darius Artem Atlas <31390399+darius-atlas@users.noreply.github.com>
2021-10-13 15:44:19 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,82 @@
+
+import React, {useRef} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import * as tf from "@tensorflow/tfjs";
+import * as facemash from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
+
+function App() {
+  // setup references
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+
+  // load facemesh
+  const runFacemesh = async () => {
+    const net = await facemash.load({
+      inputResolution: {width: 640, height: 480}, scale: 0.8
+    });
+    setInterval(() => {
+      detect(net);
+    }, 100);
+  };
+
+  // detect function
+  const detect = async (net) => {
+    if(
+        typeof webcamRef.current !== "undefined" &&
+        webcamRef.current !== null &&
+        webcamRef.current.video.readyState === 4
+    ) {
+      // get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      // set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // make detections
+      const face = await net.estimateFaces(video);
+      console.log(face);
+    }
+  };
+
+  runFacemesh();
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Webcam ref={webcamRef} style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }}/>
+        <canvas ref={canvasRef} style={{
+          position: "absolute",
+          marginLeft: "auto",
+          marginRight: "auto",
+          left: 0,
+          right: 0,
+          textAlign: "center",
+          zIndex: 9,
+          width: 640,
+          height: 480
+        }}/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
